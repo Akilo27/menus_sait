9b832a599c78993a553c117d2fef3e9eb3c730cf
Akilo <sergeencowgame@yandex.ru>
2023-10-25 16:44:04 +0300

    работа с регистрацией и авторизацией

--- sait/news/views.py
+++ sait/news/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from menu.models import Menu
 import requests
-
-
+from PIL import Image
+import  io
 def news(request):
     menus = Menu.objects.all().order_by('order')
-    url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-09-22&sortBy=publishedAt&apiKey=1dc50907bfcd45cba46078deb6b21e4d'
+    url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-09-24&sortBy=publishedAt&apiKey=1dc50907bfcd45cba46078deb6b21e4d'
 
     response = requests.get(url)
     print(response)
@@ -16,11 +16,12 @@ def news(request):
     news = []
     img = []
 
-    for article in articles:
+    for article in articles[:5]:
         if article['description'] != '[Removed]':
             news.append(article['title'])
             desc.append(article['description'])
             img.append(article['urlToImage'])
+
 
     mylist = zip(news, desc, img)
 
--- sait/relog/views.py
+++ sait/relog/views.py
@@ -1,30 +1,43 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from menu.models import Menu
+
+def relog(request):
+    menus = Menu.objects.all().order_by('order')
+    return render(request, 'relog/relog.html', {'menus': menus})
+
 
 def signup(request):
+    menus = Menu.objects.all().order_by('order')
+
+
     if request.method == 'POST':
+
         username = request.POST['username']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
 
         if password == confirm_password:
             if User.objects.filter(username=username).exists():
-                messages.error(request, 'Имя уже занято')
+                messages.error(request, 'Username is taken')
                 return redirect('signup')
             else:
                 User.objects.create_user(username=username, password=password)
-                messages.success(request, 'Аккаунт создан успешно')
+                messages.success(request, 'Account created successfully')
                 return redirect('login')
         else:
-            messages.error(request, 'неправильный пароль')
+            messages.error(request, 'Passwords do not match')
+            return redirect('signup')
     else:
-        return render(request, 'relog/signup.html')
+        return render(request, 'relog/signup.html',{'menus':menus})
 
 
 def login(request):
+    menus = Menu.objects.all().order_by('order')
+    users = User.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -32,17 +45,24 @@ def login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth_login(request, user)
-            return redirect('login')
+            return redirect('profile', name=username)
         else:
-            return render(request, 'relog/login.html')
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
+    else:
+        print(users)
+        return render(request, 'relog/login.html', {'menus': menus, 'users': users})
 
 
 @login_required
 def logout(request):
-    auth_login(request)
+    auth_logout(request)
     return redirect('login')
 
 
 @login_required
-def profile(request):
-    return render(request, 'relog/profile.html')
+def profile(request, username):
+    username = User.objects.get(username=username)
+    menus = Menu.objects.all().order_by('order')
+
+    return render(request, 'relog/profile.html', {'user': username, 'menus': menus})
